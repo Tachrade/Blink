74e515c0ca870bd82556a578a9c709cf2a0abd40
tachrade <adesokantaiwo@yahoo.com>
2020-05-26 16:43:27 +0100

    Updates

---
+++ precache-manifest.9588992bd7dc92e1e9794669dcf5b999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2dad3ed479049b8ea8332c47b59bf82",
+    "url": "/Blink.git/index.html"
+  },
+  {
+    "revision": "a006a3a5543896265575",
+    "url": "/Blink.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cab5fab8a4ab4bbeb383",
+    "url": "/Blink.git/static/js/2.fbadc43e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Blink.git/static/js/2.fbadc43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a006a3a5543896265575",
+    "url": "/Blink.git/static/js/main.4d60cbef.chunk.js"
+  },
+  {
+    "revision": "da0e5afcb618b8d8293d",
+    "url": "/Blink.git/static/js/runtime-main.67270e5c.js"
+  }
+]);
